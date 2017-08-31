59565b70cfe406482c5e62009e802aef4c55d572
Joseph <jrichardson@jmstudios.net>
2017-08-31 06:29:31 -0700

    build succeeds

---
+++ TiaInterview/GiphDisplayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GiphDisplayViewController.swift
+//  TiaInterview
+//
+//  Created by Joseph Richardson on 8/31/17.
+//  Copyright © 2017 Joseph Richardson. All rights reserved.
+//
+
+import UIKit
+
+class GiphDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TiaInterview/api.swift
@@ -0,0 +1,9 @@
+//
+//  api.swift
+//  TiaInterview
+//
+//  Created by Joseph Richardson on 8/31/17.
+//  Copyright © 2017 Joseph Richardson. All rights reserved.
+//
+
+
